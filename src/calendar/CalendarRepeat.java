799b544f676e8fadde853cf2be21f0074dc5853a
haeun <java.she123@gmail.com>
2022-03-16 14:03:04 +0900

    Add prompt to CalendarRepeat.java

--- src/calendar/CalendarRepeat.java
+++ src/calendar/CalendarRepeat.java
@@ -6,15 +6,24 @@ public class CalendarRepeat {
 
 	public static void main(String[] args) {
 	
-		System.out.println("반복할 횟수를 입력하세요.");
-		Scanner sc = new Scanner(System.in);
-		int repeat = sc.nextInt();
+		// 숫자를 입력받아 해당하는 달의 일수를 출력하는 프로그램
 		
+		Scanner sc = new Scanner(System.in);
+		String prompt = "cal> ";		
 		CalendarMethod cal = new CalendarMethod();
 		
-		for(int i=0; i < repeat; i++) {
+		int month = 1;
+		while(true) {
 			System.out.println("달을 입력하세요.");
-			int month = sc.nextInt();
+			System.out.print(prompt);
+			month = sc.nextInt();
+			if(month == -1) {
+				break;
+			}
+			
+			if(month > 12) {
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다.\n\n", month, cal.getDaysOfMonth(month));
 		}
 		
